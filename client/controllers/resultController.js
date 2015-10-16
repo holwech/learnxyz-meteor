373218008248d11e82c0c300415f22311768ce58
Joachim Holwech <joachim.holwech@gmail.com>
2015-10-16 22:08:18 +0200

    Endringer på routes

--- client/controllers/resultController.js
+++ client/controllers/resultController.js
@@ -1,13 +1,13 @@
-ResultController = RouteController.extend({
+resultController = RouteController.extend({
 	action: function() {
 		this.render("navBar", {to: "navBar"});
 		this.render("navCategory", {to: "navCategory"});
 		this.render("sideMenu", {to: "sideMenu"});
 		this.render("wordPage");
-		this.render("videos", {to: "category"})
-//		this.render(this.params.query.category, {to: "category"})
+//		this.render("videos", {to: "category"})
+		this.render(this.params.query.category, {to: "category"})
 	},
 	data: function() {
-		return {_id: Meteor.Collection.ObjectID(this.params._id), category: this.params.query.category};
+		return {_id: Meteor.Collection.ObjectID(this.params._id), category: this.params.query.category, test: "testt"};
 	}
 });
