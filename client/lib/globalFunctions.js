05b9b85b3df6098a3160ae6577436c9d8c943300
Joachim Holwech <joachim.holwech@gmail.com>
2015-11-16 21:13:23 +0100

    Search engine now supports changing between languages. Page now has full language support

--- client/lib/globalFunctions.js
+++ client/lib/globalFunctions.js
@@ -11,8 +11,15 @@ setLanguage = function(lang) {
 	if (lang !== undefined) {
 		Session.set("currentlanguage", lang);
 		TAPi18n.setLanguage(lang);
+		setSearchLanguage(lang);
 	} else {
 		Session.set("currentlanguage", "en");
 		TAPi18n.setLanguage("en");
+		setSearchLanguage("en");
 	}
 };
+
+setSearchLanguage = function(lang) {
+	WordsIndex.getComponentMethods()
+		.addProps("language", lang);
+}
--- common/collections.js
+++ common/collections.js
@@ -2,14 +2,20 @@ Urls =  new Mongo.Collection("urls");
 
 Words =  new Mongo.Collection("words");
 WordsIndex = new EasySearch.Index({
-  collection: Words,
-  fields: ["word"],
-  engine: new EasySearch.MongoDB()
+	collection: Words,
+	fields: ["word"],
+	engine: new EasySearch.MongoDB({
+		selector: function (searchObject, options, aggregation) {
+			var selector = this.defaultConfiguration().selector(searchObject, options, aggregation);
+			selector.language = options.search.props.language;
+			return selector;
+		}
+	})
 });
 
 Languages = new Mongo.Collection("languages");
 LanguagesIndex = new EasySearch.Index({
-  collection: Languages,
-  fields: ["name","nativeName"],
-  engine: new EasySearch.MongoDB()
+	collection: Languages,
+	fields: ["name","nativeName"],
+	engine: new EasySearch.MongoDB()
 });
